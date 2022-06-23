dac4738e93ac22e089a15a133ea6d0b8873f9951
Solod Sergey <Solod098@gmail.com>
2022-06-23 20:26:08 +0300

    Цикл do while и while

---
+++ js/cycle.js
@@ -0,0 +1,31 @@
+// // 1 Цикл while
+
+// let counter = 0;
+
+// while (counter < 9) {
+//   console.log('counter: ', counter);
+//   counter += 1;
+// }
+
+// // Будем заполнять места в отеле до тех пор пока текущее количество клиентов не будет равно максимально допустимому.
+
+// // 2 Цикл do while
+
+// let password = '';
+
+// do {
+//   password = prompt('Введите пароль длиннее 4-х символов', '');
+// } while (password.length < 5);
+
+// console.log('Ввели пароль: ', password);
+
+// // Конструкция do...while создает цикл, который выполняет блок кода до тех пор, пока password.length не вернёт false.
+
+// let task = '';
+
+// do {
+//   task = prompt('2+2=?', '');
+// } while (task === 4);
+
+// console.log('Ввели пароль: ', task);
+// // Конструкция do...while создает цикл, который выполняет блок кода до тех пор, пока task не вернёт false.
